4fbdda12dacb912dbbc9836b2433efd1587df3eb
drsung <drsung@ad6e42a0-9d35-419c-a641-d098a2db369c>
2013-12-07 16:48:36 +0000

    Bugfix detecting size of SD drive. WMI Win32_DiskDrive.Size is not reliable. Now using IOCTL_DISK_GET_LENGTH_INFO

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -59,5 +59,15 @@ namespace SDImager.Properties {
                 resourceCulture = value;
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Byte[].
+        /// </summary>
+        internal static byte[] MBR {
+            get {
+                object obj = ResourceManager.GetObject("MBR", resourceCulture);
+                return ((byte[])(obj));
+            }
+        }
     }
 }
